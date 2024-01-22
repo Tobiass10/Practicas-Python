7d8afb48e065d5215fddc13cac792444f6a6ee5f
Tobiass10 <155034760+Tobiass10@users.noreply.github.com>
2024-01-22 02:59:30 -0300

    Add files via upload

---
+++ Python Ejercicios Varios/Ejercicio 3.py
@@ -0,0 +1,30 @@
+#Ejercicio 3:
+"""
+/*
+ * Escribe un programa que imprima los 50 primeros números de la sucesión
+ * de Fibonacci empezando en 0.
+ * - La serie Fibonacci se compone por una sucesión de números en
+ *   la que el siguiente siempre es la suma de los dos anteriores.
+ *   0, 1, 1, 2, 3, 5, 8, 13...
+ */
+ """
+
+def fibonacci():
+    lista = []
+    for i in range(50):
+        if (i == 0):
+            lista.append(0)
+        elif(i == 1):
+            lista.append(1)
+        elif(i == 2):
+            lista.append(1)
+        else:
+            fibonacci = lista[i-1] + lista[i-2] 
+            lista.append(fibonacci)
+    return lista
+          
+def main():
+    fibo = fibonacci()
+    print(fibo)
+    
+main()
