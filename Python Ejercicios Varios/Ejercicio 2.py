3a6be077b3479942f2525e94c2b8cbc73ef5719a
Tobiass10 <155034760+Tobiass10@users.noreply.github.com>
2024-01-22 03:24:06 -0300

    Add files via upload

---
+++ Python Ejercicios Varios/Ejercicio 2.py
@@ -0,0 +1,23 @@
+"""
+/*
+ * Escribe una función que calcule y retorne el factorial de un número dado
+ * de forma recursiva.
+ */
+"""
+
+def factorial(n):
+    lista = []
+    for i in range(n):
+        if (n == 0) or (n == 1):
+            return 1
+        else:
+            return n * factorial(n-1)
+    
+
+
+def main():
+    n = int(input("Ingrese el numero de factorial a calcular: "))
+    fact = factorial(n)
+    print(fact)
+    
+main()
